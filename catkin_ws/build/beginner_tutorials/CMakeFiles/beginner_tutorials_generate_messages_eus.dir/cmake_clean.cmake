1afc4d8acac2e0cf166feb06677c2a9f88b6a28a
Michel Angelo <michelangeloaraujo77@gmail.com>
2020-11-21 12:19:50 -0300

    Turtlesim project in C++

---
+++ catkin_ws/build/beginner_tutorials/CMakeFiles/beginner_tutorials_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "/home/senick/catkin_ws/devel/share/roseus/ros/beginner_tutorials/manifest.l"
+  "CMakeFiles/beginner_tutorials_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/beginner_tutorials_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
