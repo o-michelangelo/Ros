16c2baaa866dcb43aaf68aced021c81aa84adef8
Michel Angelo <michelangeloaraujo77@gmail.com>
2020-11-21 09:48:35 -0300

    Initial commit

---
+++ build/beginner_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/senick/catkin_ws/devel/lib/beginner_tutorials/talker"
+  "/home/senick/catkin_ws/devel/lib/beginner_tutorials/talker.pdb"
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
